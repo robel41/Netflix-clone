3e8e8b164c0369a6fe74230278514d6f2cfe3e9e
robel41 <robeldebebe41@gmail.com>
2026-06-16 10:48:54 -0400

    recent update

--- nexflix-clone/src/components/Header/Header.js
+++ nexflix-clone/src/components/Header/Header.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import "./header.css"
+import NetflixLogo from "../../assets/images/NetflixLogo.png"
 import SearchIcon from '@mui/icons-material/Search';
 import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
 import AccountBoxIcon from '@mui/icons-material/AccountBox';
@@ -12,7 +13,8 @@ const Header = () => {
           <div className='header_container'>
               <div className='header_left'>
                   <ul>
-                      <li className='header_Net' ><a href="https://www.netflix.com/">Netflix</a> </li> 
+                      {/* <li className='header_Net' ><a href="https://www.netflix.com/">Netflix</a> </li> */}
+                      <li><img src={NetflixLogo} alt="Netflix Logo" width="100" /></li>
                       <li>Home</li>
                       <li>TVShows</li>
                       <li>Movies</li>
